f1d82eac5f49a77834c6101ca63b2a0f767d5540
zawkindev <zawkindev@gmail.com>
2024-07-13 22:21:26 +0500

    feat: make telegram bot handle /start command

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module subaru
 
 go 1.22.4
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,12 +1,48 @@
 package main
 
 import (
-	u "subaru/utility"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	timeShift := 1
-	filename := "luffy.srt"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	u.TimeShift(filename, int64(timeShift))
+	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if botToken == "" {
+		log.Fatal("TELEGRAM_BOT_TOKEN environment variable not set")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		msg := update.Message
+		if msg != nil {
+			if msg.IsCommand() {
+				handleStartCmd(bot, msg)
+			}
+		}
+	}
+}
+
+func handleStartCmd(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	txt := "Welcome. Send me subtitle file you want to timeshift."
+	msg := tgbotapi.NewMessage(message.Chat.ID, txt)
+	bot.Send(msg)
 }
